d1cf6e9fcbf3910ea7d389eba48500b6b0807ed9
Chwit Ruangsiri <idea.chwit@icloud.com>
2024-03-18 01:09:25 +0700

    variable-basic

---
+++ variable.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Golang! ...") // output
+
+	// constants variables
+	const money string = "ThaiBath"
+
+	// create variables
+	var firstName string = "Chwit" // string
+	lastName := "Chwit Ruangsiri"  // string
+	var age int = 27               // int || number
+	score := 112.21                // int || number
+	var isActive bool = false      // boolean
+	isPass := true                 // boolean
+
+	fmt.Println(money) //output constants
+
+	fmt.Println("ชื่อ", firstName, lastName) //output
+	fmt.Println("อายุ", age)                 //output
+	fmt.Println("มีคะแนน", score)            //output
+	fmt.Println("สถานะ", isActive)           //output
+	fmt.Println("เป็นสมาชิกหรือไม่", isPass) //output
+
+	fmt.Printf("ชื่อจริงของ %v", firstName) // output and add values variables
+
+	fmt.Printf("Type Of %T", score) // check type variables
+}
